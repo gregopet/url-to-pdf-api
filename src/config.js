879a9f10f247744cba441bcc2613de2dbee14032
Gregor Petrin <gregap@gmail.com>
2020-02-16 08:16:30 +0100

    Allow setting of interface to which Node should bind

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@
 // Env vars should be casted to correct types
 const config = {
   PORT: Number(process.env.PORT) || 9000,
+  INTERFACE: process.env.INTERFACE || '0.0.0.0',
   NODE_ENV: process.env.NODE_ENV,
   LOG_LEVEL: process.env.LOG_LEVEL,
   ALLOW_HTTP: process.env.ALLOW_HTTP === 'true',
